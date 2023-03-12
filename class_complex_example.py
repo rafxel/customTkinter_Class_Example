160a5efa704822657080356842b2cabedc33972f
Rafael <rafxel@gmail.com>
2023-03-12 16:01:16 -0300

    Update to new documentation pattern

--- class_complex_example.py
+++ class_complex_example.py
@@ -28,13 +28,11 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure((2, 3), weight=0)
         self.grid_rowconfigure((0, 1, 2), weight=1)
 
-        # place sidebar container
-        container_sidebar = customtkinter.CTkFrame(self, width=140, corner_radius=0)
-        container_sidebar.grid(row=0, column=0, rowspan=4, sticky="nsew")
-        container_sidebar.grid_rowconfigure(4, weight=1)
-
         # create sidebar frame with widgets
-        self.sidebar_frame = SideBar(container_sidebar, self)
+        self.sidebar_frame = SideBar(self)
+        self.sidebar_frame.configure(width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
 
         # create main entry and button
         self.entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
@@ -56,53 +54,39 @@ class App(customtkinter.CTk):
         self.textbox = customtkinter.CTkTextbox(self, width=250)
         self.textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
-        # place tabview
-        container_tabview = customtkinter.CTkTabview(self, width=250)
-        container_tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
         # create tabview
-        self.tabview = TabviewFrame(container_tabview)
+        self.tabview = TabviewFrame(self)
+        self.tabview.configure(width=250)
+        self.tabview.grid(row=0, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew")
 
-        # place radiobutton container
-        container_radiobuttonframe = customtkinter.CTkFrame(self)
-        container_radiobuttonframe.grid(
+        # create radiobutton frame
+        self.radiobutton_frame = RadioButtonFrame(self)
+        self.radiobutton_frame.grid(
             row=0, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew"
         )
 
-        # create radiobutton frame
-        self.radiobutton_frame = RadioButtonFrame(container_radiobuttonframe)
-
-        # place slider and progressbar frame
-        container_slider_progressbar_frame = customtkinter.CTkFrame(
-            self, fg_color="transparent"
-        )
-        container_slider_progressbar_frame.grid(
+        # create slider and progressbar frame
+        self.slider_progressbar_frame = SliderProgressbarFrame(self)
+        self.slider_progressbar_frame.configure(fg_color="transparent")
+        self.slider_progressbar_frame.grid(
             row=1, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew"
         )
-        container_slider_progressbar_frame.grid_columnconfigure(0, weight=1)
-        container_slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-
-        # create slider and progressbar frame
-        self.slider_progressbar_frame = SliderProgressbarFrame(container_slider_progressbar_frame)
-
-        # place scrollable frame
-        container_scrollable_frame = customtkinter.CTkScrollableFrame(
-            self, label_text="CTkScrollableFrame"
-        )
-        container_scrollable_frame.grid(
-            row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
-        )
-        container_scrollable_frame.grid_columnconfigure(0, weight=1)
+        self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
+        self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
 
         # create scrollable frame
-        self.scrollable_frame = ScrollableFrame(container_scrollable_frame)
+        self.scrollable_frame = ScrollableFrame(self)
+        self.scrollable_frame.configure(label_text="CTkScrollableFrame")
+        self.scrollable_frame.grid(
+            row=1, column=2, padx=(20, 0), pady=(20, 0), sticky="nsew"
+        )
+        self.scrollable_frame.grid_columnconfigure(0, weight=1)
 
         # create checkbox and switch frame
-        container_checkbox_slider_frame = customtkinter.CTkFrame(self)
-        container_checkbox_slider_frame.grid(
+        self.checkbox_slider_frame = CheckboxSwitchFrame(self)
+        self.checkbox_slider_frame.grid(
             row=1, column=3, padx=(20, 20), pady=(20, 0), sticky="nsew"
         )
-        # create checkbox and switch frame
-        self.checkbox_slider_frame = CheckboxSwitchFrame(container_checkbox_slider_frame)
 
         # set default values
         self.textbox.insert(
@@ -153,44 +137,45 @@ class SideBar(customtkinter.CTkFrame):
     Args:
         customtkinter (_type_): CustomTkinter component
     """
-    def __init__(self, parent, controller):
-        customtkinter.CTkFrame.__init__(self, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
         logo_label = customtkinter.CTkLabel(
-            parent,
+            self,
             text="CustomTkinter",
             font=customtkinter.CTkFont(size=20, weight="bold"),
         )
         logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
         sidebar_button_1 = customtkinter.CTkButton(
-            parent, command=controller.sidebar_button_event
+            self, command=self.sidebar_button_event
         )
         sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
         sidebar_button_2 = customtkinter.CTkButton(
-            parent, command=controller.sidebar_button_event
+            self, command=self.sidebar_button_event
         )
         sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
         sidebar_button_3 = customtkinter.CTkButton(
-            parent, command=controller.sidebar_button_event
+            self, command=self.sidebar_button_event
         )
         sidebar_button_3.grid(row=3, column=0, padx=20, pady=10)
         appearance_mode_label = customtkinter.CTkLabel(
-            parent, text="Appearance Mode:", anchor="w"
+            self, text="Appearance Mode:", anchor="w"
         )
         appearance_mode_label.grid(row=5, column=0, padx=20, pady=(10, 0))
         appearance_mode_optionemenu = customtkinter.CTkOptionMenu(
-            parent,
+            self,
             values=["Light", "Dark", "System"],
-            command=controller.change_appearance_mode_event,
+            command=self.change_appearance_mode_event,
         )
         appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
         scaling_label = customtkinter.CTkLabel(
-            parent, text="UI Scaling:", anchor="w"
+            self, text="UI Scaling:", anchor="w"
         )
         scaling_label.grid(row=7, column=0, padx=20, pady=(10, 0))
         scaling_optionemenu = customtkinter.CTkOptionMenu(
-            parent,
+            self,
             values=["80%", "90%", "100%", "110%", "120%"],
-            command=controller.change_scaling_event,
+            command=self.change_scaling_event,
         )
         scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
 
@@ -198,6 +183,32 @@ class SideBar(customtkinter.CTkFrame):
         sidebar_button_3.configure(state="disabled", text="Disabled CTkButton")
         appearance_mode_optionemenu.set("Dark")
         scaling_optionemenu.set("100%")
+    
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        """
+        Changes the appearence of the CustomTkinter GUI to one of three
+        available modes: "System" (standard), "Dark", "Light"
+
+        Args:
+            new_appearance_mode (str): "System" (standard), "Dark", "Light"
+        """
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        """
+        Changes the scale of the GUI
+
+        Args:
+            new_scaling (str): string in a percentage structure 1%-100%
+        """
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+    def sidebar_button_event(self):
+        """
+        Sidebar button event
+        """
+        print("sidebar_button click")
 
 
 class RadioButtonFrame(customtkinter.CTkFrame):
@@ -207,26 +218,26 @@ class RadioButtonFrame(customtkinter.CTkFrame):
     Args:
         customtkinter (_type_): CustomTkinter component
     """
-    def __init__(self, parent):
-        customtkinter.CTkFrame.__init__(self, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         self.radio_var = tkinter.IntVar(value=0)
         self.label_radio_group = customtkinter.CTkLabel(
-            master=parent, text="CTkRadioButton Group:"
+            master=self, text="CTkRadioButton Group:"
         )
         self.label_radio_group.grid(
             row=0, column=2, columnspan=1, padx=10, pady=10, sticky=""
         )
         self.radio_button_1 = customtkinter.CTkRadioButton(
-            master=parent, variable=self.radio_var, value=0
+            master=self, variable=self.radio_var, value=0
         )
         self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
         self.radio_button_2 = customtkinter.CTkRadioButton(
-            master=parent, variable=self.radio_var, value=1
+            master=self, variable=self.radio_var, value=1
         )
         self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
         self.radio_button_3 = customtkinter.CTkRadioButton(
-            master=parent, variable=self.radio_var, value=2
+            master=self, variable=self.radio_var, value=2
         )
         self.radio_button_3.grid(row=3, column=2, pady=10, padx=20, sticky="n")
 
@@ -241,13 +252,13 @@ class ScrollableFrame(customtkinter.CTkScrollableFrame):
     Args:
         customtkinter (_type_): CustomTkinter component
     """
-    def __init__(self, parent):
-        customtkinter.CTkScrollableFrame.__init__(self, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         scrollable_frame_switches = []
         for i in range(100):
             switch = customtkinter.CTkSwitch(
-                master=parent, text=f"CTkSwitch {i}"
+                master=self, text=f"CTkSwitch {i}"
             )
             switch.grid(row=i, column=0, padx=10, pady=(0, 20))
             scrollable_frame_switches.append(switch)
@@ -264,14 +275,14 @@ class CheckboxSwitchFrame(customtkinter.CTkFrame):
     Args:
         customtkinter (_type_): CustomTkinter component
     """
-    def __init__(self, parent):
-        customtkinter.CTkFrame.__init__(self, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        checkbox_1 = customtkinter.CTkCheckBox(master=parent)
+        checkbox_1 = customtkinter.CTkCheckBox(master=self)
         checkbox_1.grid(row=1, column=0, pady=(20, 0), padx=20, sticky="n")
-        checkbox_2 = customtkinter.CTkCheckBox(master=parent)
+        checkbox_2 = customtkinter.CTkCheckBox(master=self)
         checkbox_2.grid(row=2, column=0, pady=(20, 0), padx=20, sticky="n")
-        checkbox_3 = customtkinter.CTkCheckBox(master=parent)
+        checkbox_3 = customtkinter.CTkCheckBox(master=self)
         checkbox_3.grid(row=3, column=0, pady=20, padx=20, sticky="n")
 
         # set default values
@@ -286,35 +297,35 @@ class SliderProgressbarFrame(customtkinter.CTkFrame):
     Args:
         customtkinter (_type_): CustomTkinter component
     """
-    def __init__(self, parent):
-        customtkinter.CTkFrame.__init__(self, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
         seg_button_1 = customtkinter.CTkSegmentedButton(
-            parent
+            self
         )
         seg_button_1.grid(
             row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
         )
-        progressbar_1 = customtkinter.CTkProgressBar(parent)
+        progressbar_1 = customtkinter.CTkProgressBar(self)
         progressbar_1.grid(
             row=1, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
         )
-        progressbar_2 = customtkinter.CTkProgressBar(parent)
+        progressbar_2 = customtkinter.CTkProgressBar(self)
         progressbar_2.grid(
             row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew"
         )
         slider_1 = customtkinter.CTkSlider(
-            parent, from_=0, to=1, number_of_steps=4
+            self, from_=0, to=1, number_of_steps=4
         )
         slider_1.grid(row=3, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
         slider_2 = customtkinter.CTkSlider(
-            parent, orientation="vertical"
+            self, orientation="vertical"
         )
         slider_2.grid(
             row=0, column=1, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns"
         )
         progressbar_3 = customtkinter.CTkProgressBar(
-            parent, orientation="vertical"
+            self, orientation="vertical"
         )
         progressbar_3.grid(
             row=0, column=2, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns"
@@ -342,36 +353,36 @@ class TabviewFrame(customtkinter.CTkTabview):
     Args:
         customtkinter (_type_): CustomTkinter component
     """
-    def __init__(self, parent):
-        customtkinter.CTkTabview.__init__(self, parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-        parent.add("CTkTabview")
-        parent.add("Tab 2")
-        parent.add("Tab 3")
-        parent.tab("CTkTabview").grid_columnconfigure(
+        self.add("CTkTabview")
+        self.add("Tab 2")
+        self.add("Tab 3")
+        self.tab("CTkTabview").grid_columnconfigure(
             0, weight=1
         )  # configure grid of individual tabs
-        parent.tab("Tab 2").grid_columnconfigure(0, weight=1)
+        self.tab("Tab 2").grid_columnconfigure(0, weight=1)
 
         optionmenu_1 = customtkinter.CTkOptionMenu(
-            parent.tab("CTkTabview"),
+            self.tab("CTkTabview"),
             dynamic_resizing=False,
             values=["Value 1", "Value 2", "Value Long Long Long"],
         )
         optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
         combobox_1 = customtkinter.CTkComboBox(
-            parent.tab("CTkTabview"),
+            self.tab("CTkTabview"),
             values=["Value 1", "Value 2", "Value Long....."],
         )
         combobox_1.grid(row=1, column=0, padx=20, pady=(10, 10))
         string_input_button = customtkinter.CTkButton(
-            parent.tab("CTkTabview"),
+            self.tab("CTkTabview"),
             text="Open CTkInputDialog",
             command=self.open_input_dialog_event,
         )
         string_input_button.grid(row=2, column=0, padx=20, pady=(10, 10))
         label_tab_2 = customtkinter.CTkLabel(
-            parent.tab("Tab 2"), text="CTkLabel on Tab 2"
+            self.tab("Tab 2"), text="CTkLabel on Tab 2"
         )
         label_tab_2.grid(row=0, column=0, padx=20, pady=20)
 
